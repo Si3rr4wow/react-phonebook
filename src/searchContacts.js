b23c8cab92cfd67032ffd9fa5c1c0d239972d2fb
Mark <si3rr4wow@gmail.com>
2020-12-13 16:19:07 +0000

    rewrite SearchContacts as a functional component

--- src/searchContacts.js
+++ src/searchContacts.js
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBox from "./searchBox";
 import Results from "./results";
 import getResults from "./api/getResults";
 
-class SearchContacts extends React.Component {
-  constructor(props) {
-    super(props);
-    const { terms } = this.props.params || {};
-    const searchActive = Boolean(terms);
-    this.state = {
-      currentSearch: searchActive ? terms : "",
-      results: searchActive ? getResults(terms) : [],
-      searchActive: searchActive,
-    };
-    this.onSubmit = this.onSubmit.bind(this);
+const SearchClients = props => {
+  const { terms } = props.params || {};
+  const searchActive = Boolean(terms);
+
+  const [state, setState] = useState({
+    currentSearch: searchActive ? terms : "",
+    results: searchActive ? getResults(terms) : [],
+    searchActive: searchActive,
+  })
+
+  const clearSearch = () => {
+    setState({
+      searchActive: false,
+      currentSearch: "",
+      results: [],
+    });
   }
 
-  onSubmit(searchStr) {
+  const onSubmit = (searchStr) => {
     if (searchStr.length === 0) {
-      return this.clearSearch();
+      return clearSearch();
     }
-    if (this.state.currentSearch !== searchStr) {
-      this.setState({
+    if (state.currentSearch !== searchStr) {
+      setState({
         searchActive: true,
         currentSearch: searchStr,
         results: getResults(searchStr),
@@ -29,25 +34,15 @@ class SearchContacts extends React.Component {
     }
   }
 
-  clearSearch() {
-    this.setState({
-      searchActive: false,
-      currentSearch: "",
-      results: [],
-    });
-  }
-
-  render() {
-    return (
-      <section>
-        <SearchBox
-          onSubmit={this.onSubmit}
-          currentSearch={this.state.currentSearch}
-        />
-        {this.state.searchActive ? <Results data={this.state.results} /> : ""}
-      </section>
-    );
-  }
+  return (
+    <section>
+      <SearchBox
+        onSubmit={onSubmit}
+        currentSearch={state.currentSearch}
+      />
+      {state.searchActive ? <Results data={state.results} /> : ""}
+    </section>
+  );
 }
 
-export default SearchContacts;
+export default SearchClients
